14a19cf0922524b9d5fd377dd4160b484191fbe4
Sahaj Mistry <sahajmistry005@gmail.com>
2021-06-13 18:46:57 +0530

    h-link in title

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ export default function Header(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">{props.title}</Link>
+                <Link className="navbar-brand" to="/Todo-List/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
